8a564cc0f7715ad7bc2b648b20a780989319aa9f
Joseph Robinson <smartech56@gmail.com>
2018-01-02 17:59:56 -0700

    developed extras.bat

---
+++ dev/extras.bat
@@ -0,0 +1,11 @@
+echo showing added commands:
+echo 7z - 7Zip for command prompt
+echo aapt - List contents of Zip-compatible archive
+echo adb - Android Debug Bridge
+echo dmtracedump - an android utility to trace dumps
+echo dpinst - installs android drivers for adb and fastboot
+echo etc1tool.exe - encodes and decodes files
+echo fastboot - allows you to recover or customize android but the device needs to be in fastboot mode
+echo hprof-conv - used to convert hprov files
+echo sqlite3 - starts sqlite terminal
+echo tar - GNU `tar' saves many files together into a single tape or disk archive, and can restore individual files from the archive.
