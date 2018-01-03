2e4c4e04790fd9a8732e2b5ee2b05541d560fa6f
Joseph Robinson <smartech56@gmail.com>
2018-01-02 18:05:38 -0700

    added command twrpinstall to extras.bat

--- extras.bat
+++ extras.bat
@@ -6,6 +6,7 @@ echo dmtracedump - an android utility to trace dumps
 echo dpinst - installs android drivers for adb and fastboot
 echo etc1tool.exe - encodes and decodes files
 echo fastboot - allows you to recover or customize android but the device needs to be in fastboot mode
+echo twrpsetup - uses fastboot to automatically flash twrp to recovery partition on an android device
 echo hprof-conv - used to convert hprov files
 echo sqlite3 - starts sqlite terminal
 echo tar - GNU `tar' saves many files together into a single tape or disk archive, and can restore individual files from the archive.
